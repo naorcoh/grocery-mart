e25086aa66d7acf67232eed40e66ed6f65d5f312
Naorcoh <naorcohen92@gmail.com>
2022-12-06 01:49:00 +0200

    init grocery mart web application projectc

---
+++ src/main/java/com/naorcoh/grocerymart/GroceryMartApplication.java
@@ -0,0 +1,13 @@
+package com.naorcoh.grocerymart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryMartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naorcoh/grocerymart/GroceryMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naorcoh.grocerymart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
